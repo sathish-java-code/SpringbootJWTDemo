0ef517a0b60e4ceb561080343e058adf97f0a838
sathish-java-code <sathishmadhu@hotmail.com>
2025-05-15 16:38:39 +0530

    sample code added

--- src/main/java/com/example/jwtdemo/jwtdemo/controller/HelloController.java
+++ src/main/java/com/example/jwtdemo/jwtdemo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, JWT Demo Created Sucessfully!";
+        return "Hello, JWT Demo Created !";
     }
 }
