49739a0125b8f6a7383e5e2791699ee215bae45f
sathish-java-code <sathishmadhu@hotmail.com>
2025-05-15 16:26:43 +0530

    sample code added

---
+++ src/main/java/com/example/jwtdemo/jwtdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.jwtdemo.jwtdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, JWT Demo Created Sucessfully!";
+    }
+}
--- src/test/java/com/example/jwtdemo/jwtdemo/JwtdemoApplicationTests.java
+++ src/test/java/com/example/jwtdemo/jwtdemo/JwtdemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.jwtdemo.jwtdemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JwtdemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.jwtdemo.jwtdemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JwtdemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
